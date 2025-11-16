93f11c44fefaa26a38769befacc84490ee35c1ca
zenidunnn <malikzhafran52@gmail.com>
2025-11-16 20:39:19 +0700

    Revert "Delete TPMODUL3_MALIK directory"
    This reverts commit d2a55741ba8b34d424d8c4ac996cd50212433329.

---
+++ TPMODUL3_MALIK/app/Models/Kucing.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Kucing extends Model {
+    use HasFactory;
+
+    protected $fillable = [
+        'nama_kucing',
+        'ras',
+        'warna_bulu',
+        'usia',
+        'deskripsi'
+    ];
+}
