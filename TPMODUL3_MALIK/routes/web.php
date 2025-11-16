2f6b6b9b2d66ea096e63d678b16f8416077e591b
zenidunnn <malikzhafran52@gmail.com>
2025-11-16 20:42:59 +0700

    Delete TPMODUL3_MALIK directory

--- TPMODUL3_MALIK/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\KucingController;
-
-Route::get('/kucing', [KucingController::class, 'index']);
-Route::get('/kucing/{id}', [KucingController::class, 'show']);
